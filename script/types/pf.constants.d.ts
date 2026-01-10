be12a1047af9573ce52a9aaca05c57114c2d6575
kareobana <kareobana@atarabi.com>
2026-01-10 11:23:11 +0900

    update urls

--- script/types/pf.constants.d.ts
+++ script/types/pf.constants.d.ts
@@ -1,6 +1,6 @@
 declare namespace PF {
 
-    // https://ae-plugins.docsforadobe.dev/effect-basics/parameters.html#effect-basics-parameters-parameter-types
+    // https://ae-plugins.docsforadobe.dev/effect-basics/parameters/#parameter-types
     const enum ParamType {
         Reserved = -1,
         Layer = 0,
@@ -24,7 +24,7 @@ declare namespace PF {
         Point3D,
     }
 
-    // https://ae-plugins.docsforadobe.dev/effect-basics/PF_ParamDef.html#effect-basics-pf-paramdef-parameter-ui-flags
+    // https://ae-plugins.docsforadobe.dev/effect-basics/PF_ParamDef/?h=pf_paramdef#parameter-ui-flags
     const enum ParamUIFlags {
         None = 0,
         Topic = 1 << 0,
@@ -39,7 +39,7 @@ declare namespace PF {
         Invisible = 1 << 9,
     }
 
-    // https://ae-plugins.docsforadobe.dev/effect-basics/PF_ParamDef.html#effect-basics-pf-paramdef-parameter-flags
+    // https://ae-plugins.docsforadobe.dev/effect-basics/PF_ParamDef/?h=pf_paramdef#parameter-flags
     const enum ParamFlags {
         None = 0,
         Reserved1 = 1 << 0,
@@ -56,7 +56,7 @@ declare namespace PF {
         SkipRevealWhenUnhiddn = 1 << 9,
     }
 
-    // https://ae-plugins.docsforadobe.dev/effect-basics/PF_ParamDef.html#pf-valuedisplayflags
+    // https://ae-plugins.docsforadobe.dev/effect-basics/PF_ParamDef/?h=pf_paramdef#pf_valuedisplayflags
     const enum ValueDisplayFlags {
         None = 0,
         Percent = 1 << 0,
@@ -65,7 +65,7 @@ declare namespace PF {
         Reverse = 1 << 3,
     }
 
-    // https://ae-plugins.docsforadobe.dev/effect-basics/PF_EffectWorld.html#pf-effectworld-structure
+    // https://ae-plugins.docsforadobe.dev/effect-basics/PF_EffectWorld/#pf_effectworld-structure
     const enum LayerDefault {
         Myself = -1,
         None = 0,
