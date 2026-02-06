ed6e8ad6e470dc5846476458a9db68e3b3083efc
kareobana <kareobana@atarabi.com>
2026-02-07 07:11:43 +0900

    refactor regexp

--- script/src/Startup/@save_to_desktop.tsx
+++ script/src/Startup/@save_to_desktop.tsx
@@ -1,7 +1,8 @@
 /**
- * @save_to_desktop v1.0.1
+ * @save_to_desktop v1.1.0
  * 
- *      v1.0.1(2024/02/13) Fix dynamic link bug
+ *      v1.1.0(2024/02/13) Refactored the regex for filenames
+ *      v1.0.1(2024/02/13) Fixed a dynamic link bug
  *      v1.0.0(2023/09/16)
  */
 (() => {
@@ -13,7 +14,7 @@
     Atarabi.keyboard.hook({ code: 'S', altKey: true }, ctx => {
         const comp = Atarabi.comp.getMostRecentlyUsedComp();
         if (comp) {
-            const file = new File(`${Folder.desktop.absoluteURI}/${comp.name.replace(/\//g, '-')}_${Date.now()}.png`);
+            const file = new File(`${Folder.desktop.absoluteURI}/${comp.name.replace(/[<>:"\/\\|?*]/g, '-')}_${Date.now()}.png`);
             Atarabi.comp.saveFrameToPng(comp, file);
         }
         return true;
